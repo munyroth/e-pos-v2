9fd62a1c69733240d471583f1e1ee00e3d0d79f9
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2024-05-03 17:07:55 +0700

    -Fixing build error

--- src/hooks/useGetDataList.js
+++ src/hooks/useGetDataList.js
@@ -45,7 +45,7 @@ const useGetDataList = (url, openModalAdd, openModalDelete) => {
             isMounted = false;
             controller.abort();
         };
-    }, [url, meta.page, axiosPrivate, openModalAdd, openModalDelete]);
+    }, [url, meta.page, auth.role, axiosPrivate, openModalAdd, openModalDelete]);
 
     return [data, meta, isLoading, setData, setMeta, setIsLoading];
 };
