801d68d28f5ae72f71dca1dd9040107ba46a7260
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2024-05-03 10:33:40 +0700

    -improving navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,6 +5,8 @@ import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import Cookies from "js-cookie";
 import {CheckIcon} from "@heroicons/react/20/solid";
 import useAuth from "../../hooks/useAuth";
+import useGetDataObject from "../../hooks/useGetDataObject";
+import useGetDataList from "../../hooks/useGetDataList";
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -14,51 +16,22 @@ export default function Navbar() {
     const axiosPrivate = useAxiosPrivate();
     const {auth, logout} = useAuth();
 
-    const [user, setUser] = useState(null);
-    const [shops, setShops] = useState([{id: 1, name: null}]);
-    const [selected, setSelected] = useState(shops[0])
+    // eslint-disable-next-line
+    const [user, isLoadingUser, setUser, setIsLoadingUser] = useGetDataObject('/user')
+    // eslint-disable-next-line
+    const [shops, meta, isLoadingShops, setShops, setMeta, setIsLoadingShops] = useGetDataList('/business')
+    const [selected, setSelected] = useState({
+        id: 0,
+        name: ""
+    })
 
     useEffect(() => {
+        console.log("shops", shops)
         let shopId = Cookies.get('shopId');
-        if (shopId && shops[0].name) {
+        if (shopId && !isLoadingShops) {
             setSelected(shops.find(shop => shop.id === parseInt(shopId)))
         }
-    }, [shops]);
-
-    useEffect(() => {
-        let isMounted = true;
-        const controller = new AbortController();
-
-        const getUser = async () => {
-            try {
-                const res = await axiosPrivate.get('/user', {
-                    signal: controller.signal
-                });
-                isMounted && setUser(res.data.data)
-            } catch (err) {
-
-            }
-        }
-
-        const getShop = async () => {
-            try {
-                const res = await axiosPrivate.get('/business', {
-                    signal: controller.signal
-                });
-                isMounted && setShops(res.data.data)
-            } catch (err) {
-
-            }
-        }
-
-        getUser();
-        getShop();
-
-        return () => {
-            isMounted = false;
-            controller.abort();
-        }
-    }, [axiosPrivate]);
+    }, [shops, isLoadingShops]);
 
     return (
         <Disclosure as="nav" className="px-4 border-b dark:bg-gray-800 dark:border-gray-700">
@@ -73,7 +46,7 @@ export default function Navbar() {
                                     <Listbox.Button
                                         className="relative w-full cursor-pointer rounded-md py-1.5 px-3 text-left sm:leading-6 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                                         <span className="flex items-center">
-                                            {selected.name
+                                            {!isLoadingShops
                                                 ? <span className="block truncate">{selected.name}</span>
                                                 : <svg aria-hidden="true"
                                                        className="inline w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-500"
@@ -85,7 +58,7 @@ export default function Navbar() {
                                                     <path
                                                         d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
                                                         fill="currentFill"/>
-                                                </svg>
+                                                </svg> // loading icon
                                             }
                                         </span>
                                     </Listbox.Button>
@@ -157,7 +130,15 @@ export default function Navbar() {
                                     <div className="font-medium dark:text-white">
                                         <div>{user?.name}</div>
                                         <div className="text-sm text-gray-500 dark:text-gray-400">
-                                            {user?.role}
+                                            {user?.role === "admin" ? (
+                                                "ម្ចាស់ហាង"
+                                            ) : user?.role === "manager" ? (
+                                                "អ្នកគ្រប់គ្រង"
+                                            ) : user?.role === "sale" ? (
+                                                "អ្នកលក់"
+                                            ) : (
+                                                "សមាជិក"
+                                            )}
                                         </div>
                                     </div>
                                 </div>
