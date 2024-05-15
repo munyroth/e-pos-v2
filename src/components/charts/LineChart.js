f8a05d47c22e335f17a055c05df57a9ed674c1fe
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2024-05-15 19:13:54 +0700

    -Fixing barchart responsive

--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -4,182 +4,182 @@ import Chart from "react-apexcharts";
 class LineChart extends Component {
 
     render() {
-        const data = Object.entries(this.props.data).map(([date, value]) => ({ date, value: parseInt(value) }));
+        const data = Object.entries(this.props.data).map(([date, value]) => ({date, value: parseInt(value)}));
         let max = data.reduce((max, p) => p.value > max ? p.value : max, data[0]?.value);
 
         return (
             <div className="flex justify-center w-full">
-                
                 <Chart
                     series={[
                         {
                             name: "ថ្ងៃនេះ",
-                            data: data.map(({value }) => value)
+                            data: data.map(({value}) => value)
                         }
                     ]}
                     type="area"
-                    options={{
-                        chart: {
-                            type: "area",
-                            fontFamily: 'Helvetica, Arial, sans-serif',
-                            foreColor: '#808080',
-                            dropShadow: {
-                                enabled: true,
-                                color: ['#32a852'],
-                                top: 5,
-                                left: 5,
-                                blur: 2,
-
-                            },
-                            zoom: {
+                    options={
+                        {
+                            dataLabels: {
                                 enabled: false
                             },
-                            toolbar: {
-                                show: true
-                            }
-                        },
-                        fill: {
-                            type: 'gradient',
-                            gradient: {
-                                shadeIntensity: 1,
-                                opacityFrom: 0.4,
-                                opacityTo: 0.2,
-                                stops: [0, 90, 100]
-                            }
-                        },
-                        colors: ['#32a852'],
-                        dataLabels: {
-                            enabled: false
-                        },
-                        stroke: {
-                            curve: 'smooth',
-                            width: 2,
-                        },
-                        // title: {
-                        //     text: this.props.title,
-                        //     style: {
-                        //         fontSize: '18px',
-                        //         fontWeight:  '700',
-                        //         color:  '#111827'
-                        //       },
-                        // },
-                        grid: {
-                            strokeDashArray: 10
-                        },
-                        markers: {
-                            size: 4,
+                            xaxis: {
+                                axisBorder: {
+                                    show: false
+                                },
+                                axisTicks: {
+                                    show: false
+                                },
+                                categories: data.map(({date}) => date),
+                            },
+                            grid: {
+                                strokeDashArray: 10
+                            },
+                            legend: {
+                                position: 'top',
+                                horizontalAlign: 'right',
+                                floating: true,
+                                offsetY: -40,
+                                offsetX: -5
+                            },
+                            responsive: [
+                                {
+                                    breakpoint: 1600,
+                                    options: {
+                                        chart: {
+                                            width: "1050",
+                                            height: "300"
+                                        },
+                                    }
+                                },
+                                {
+                                    breakpoint: 1500,
+                                    options: {
+                                        chart: {
+                                            width: "1000",
+                                            height: "300"
+                                        },
+                                    }
+                                },
+                                {
+                                    breakpoint: 1400,
+                                    options: {
+                                        chart: {
+                                            width: "950",
+                                            height: "300"
+                                        },
+                                    }
+                                },
+                                {
+                                    breakpoint: 1300,
+                                    options: {
+                                        chart: {
+                                            width: "900",
+                                            height: "300"
+                                        },
+                                    }
+                                },
+                                {
+                                    breakpoint: 1200,
+                                    options: {
+                                        chart: {
+                                            width: "800",
+                                            height: "300"
+                                        },
+                                    }
+                                },
+                                {
+                                    breakpoint: 1100,
+                                    options: {
+                                        chart: {
+                                            width: "700",
+                                            height: "300"
+                                        },
+                                    }
+                                },
+                                {
+                                    breakpoint: 1000,
+                                    options: {
+                                        chart: {
+                                            width: "600",
+                                            height: "300"
+                                        },
+                                    }
+                                },
+                                {
+                                    breakpoint: 900,
+                                    options: {
+                                        chart: {
+                                            width: "550",
+                                            height: "300"
+                                        },
+                                    }
+                                },
+                            ],
+                            fill: {
+                                type: 'gradient',
+                                gradient: {
+                                    shadeIntensity: 1,
+                                    opacityFrom: 0.4,
+                                    opacityTo: 0.2,
+                                    stops: [0, 90, 100]
+                                }
+                            },
+                            markers: {
+                                size: 4,
+                                colors: ['#32a852'],
+                                strokeWidth: 2,
+                            },
+                            chart: {
+                                type: "area",
+                                fontFamily: 'Helvetica, Arial, sans-serif',
+                                foreColor: '#808080',
+                                dropShadow: {
+                                    enabled: true,
+                                    color: ['#32a852'],
+                                    top: 5,
+                                    left: 5,
+                                    blur: 2,
+
+                                },
+                                zoom: {
+                                    enabled: false
+                                },
+                                toolbar: {
+                                    show: true
+                                }
+                            },
+                            yaxis: {
+                                tickAmount: 4,
+                                min: 0,
+                                max: max
+                            },
+                            stroke: {
+                                curve: 'smooth',
+                                width: 2,
+                            },
                             colors: ['#32a852'],
-                            strokeWidth: 2,
-                        },
-                        xaxis: {
-                            axisBorder: {
-                                show: false
-                            },
-                            axisTicks: {
-                                show: false
-                            },
-                            categories: data.map(({ date}) => date),
-                        },
-                        yaxis: {
-                            tickAmount: 4,
-                            min: 0,
-                            max: max
-                        },
-                        legend: {
-                            position: 'top',
-                            horizontalAlign: 'right',
-                            floating: true,
-                            offsetY: -40,
-                            offsetX: -5
-                        },
-                        // theme: {
-                        //     mode: 'light',
-                        //     palette: 'palette1',
-                        //     monochrome: {
-                        //         enabled: false,
-                        //         color: '#255aee',
-                        //         shadeTo: 'light',
-                        //         shadeIntensity: 0.65
-                        //     },
-                        // },
-                        responsive: [
-                            {
-                                breakpoint: 1600,
-                                options: {
-                                    chart: {
-                                        width: "1050",
-                                        height: "300"
-                                    },
-                                }
-                            },
-                            {
-                                breakpoint: 1500,
-                                options: {
-                                    chart: {
-                                        width: "1000",
-                                        height: "300"
-                                    },
-                                }
-                            },
-                            {
-                                breakpoint: 1400,
-                                options: {
-                                    chart: {
-                                        width: "950",
-                                        height: "300"
-                                    },
-                                }
-                            },
-                            {
-                                breakpoint: 1300,
-                                options: {
-                                    chart: {
-                                        width: "900",
-                                        height: "300"
-                                    },
-                                }
-                            },
-                            {
-                                breakpoint: 1200,
-                                options: {
-                                    chart: {
-                                        width: "800",
-                                        height: "300"
-                                    },
-                                }
-                            },
-                            {
-                                breakpoint: 1100,
-                                options: {
-                                    chart: {
-                                        width: "700",
-                                        height: "300"
-                                    },
-                                }
-                            },
-                            {
-                                breakpoint: 1000,
-                                options: {
-                                    chart: {
-                                        width: "600",
-                                        height: "300"
-                                    },
-                                }
-                            },
-                            {
-                                breakpoint: 900,
-                                options: {
-                                    chart: {
-                                        width: "550",
-                                        height: "300"
-                                    },
-                                }
-                            },
-                        ]
-                    }}
+                            // title: {
+                            //     text: this.props.title,
+                            //     style: {
+                            //         fontSize: '18px',
+                            //         fontWeight:  '700',
+                            //         color:  '#111827'
+                            //       },
+                            // },
+                            // theme: {
+                            //     mode: 'light',
+                            //     palette: 'palette1',
+                            //     monochrome: {
+                            //         enabled: false,
+                            //         color: '#255aee',
+                            //         shadeTo: 'light',
+                            //         shadeIntensity: 0.65
+                            //     },
+                            // },
+                        }
+                    }
                 />
             </div>
-
         );
     }
 }
